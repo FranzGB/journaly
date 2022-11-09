3181ee294145b72827f2cdc403ff59a5332f27b5
Franz Guzmán <fguzman@stackbuilders.com>
2022-11-08 19:12:45 -0500

    Initial commit

---
+++ journaly_app/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for journaly project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "journaly.settings")
+
+application = get_asgi_application()
