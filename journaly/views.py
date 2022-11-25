ec8a73b96e68e694d5842cb06fd3be7341feecb3
Franz Guzmán <fguzman@stackbuilders.com>
2022-11-24 23:34:04 -0500

    Add base html view

--- journaly/views.py
+++ journaly/views.py
@@ -1,5 +1,20 @@
 from .models import Journal, JournalEntry
 from django.views.generic import ListView
+from django.shortcuts import render
+
+
+def base(request):
+    """View function for home page of site"""
+
+    # Generate counts of some of the main objects
+    num_journals = Journal.objects.all().count()
+    num_entries = JournalEntry.objects.all().count()
+    context = {
+        "num_journals": num_journals,
+        "num_entries": num_entries,
+    }
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, "base.html", context=context)
 
 
 class JournalListView(ListView):
